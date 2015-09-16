e40d27b400206f0bf38d57803932a317096d270c
Rachelle Herrera <rachelle.herera@gmail.com>
2015-09-15 23:20:44 -0700

    added methods for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,56 @@
-class UsersController < ApplicationController
+ class UsersController < ApplicationController
+  
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
-  def edt
+  def create
+    @user = User.new(user_params)
+    if @user.save 
+      session[:user_id] = @user.id
+      redirect_to users_path
+    else 
+      render 'new'
+    end
+  end
+
+
+  def edit 
+    @user = User.find(params[:id])
+  end
+
+   def show
+    @user = User.find(params[:id])
+    @user_photos = @user.photos(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user = @user.update_attributes(user_params)
+    flash[:success] = "Location Updated"
+      redirect_to user_path(current_user)
+    else
+      render 'edit'
+    end
   end
 
-  def show
+  def destroy 
+    user = User.find(params[:id])
+    if user.destroy
+      redirect_to root_path
+    else 
+      redirect_to user_path(current_user)
+    end
   end
 
-  def destroy
+  private 
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :lat, :lng)
   end
+  
 end
