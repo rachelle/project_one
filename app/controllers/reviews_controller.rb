f1473eefa9c6c0addd39573055cf11c0e9525731
Rachelle Herrera <rachelle.herera@gmail.com>
2015-09-15 23:26:13 -0700

    added methods for reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,30 @@
 class ReviewsController < ApplicationController
+  
   def index
+   @reviews = photo.reviews
   end
 
   def new
+    #creates a review for the given photo
+    @review = @photo.reviews.new
+    @review.user = current_user.id
+  end
+
+  def create
+    @review = @photo.reviews.new(review_params)
+    #for the new review sets the user to be current_user
+    @review.user = current_user
+    if @review.save
+      flash[:success] = "Comment posted!"
+      redirect_to photo_path(@photo.id)
+    else 
+      render 'new'
+    end
   end
 
   def show
+    @photo = Photo.find(params[:photo_id])
+    @review = photo.reviews.find(params[:id])
   end
 
   def edit
@@ -15,5 +34,16 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
+    @photo = Photo.find(params[:photo_id])
+    @review = Review.find(params[:id])
+    @review.destroy 
+    redirect_to photo_path(@photo.id)
   end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+
 end
