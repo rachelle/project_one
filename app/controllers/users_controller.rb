cd8f8a5a7fb48ca2a0adf52c696fb600ac242256
Rachelle Herrera <rachelle.herera@gmail.com>
2015-09-15 23:07:40 -0700

    added users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edt
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   
 
+  get 'users/index'
+
+  get 'users/new'
+
+  get 'users/edt'
+
+  get 'users/update'
+
+  get 'users/show'
+
+  get 'users/destroy'
+
    root 'sessions#index'
 
 end
