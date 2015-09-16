75b0306c01e4a39ee0476856354f72b90d2974d7
Rachelle Herrera <rachelle.herera@gmail.com>
2015-09-15 23:23:05 -0700

    added methods for photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,19 +1,57 @@
 class PhotosController < ApplicationController
+  
   def index
-  end
-
-  def new
+    @photos = Photo.all
   end
 
   def show
+    @reviews = @photo.reviews.order('created_at DESC')
+    @review = Review.new
+    @review.user = current_user
+  end
+
+  def new
+    @photo = current_user.photos.new
+  end
+
+ def create
+    @photo = current_user.photos.create(photo_params)
+    if @photo.save
+       redirect_to photos_path(current_user)
+    else 
+       render 'new'
+    end
   end
 
   def edit
+    @photo = Photo.find(params[:id])
   end
 
   def update
+    if @photo.update(photo_params)
+      flash[:success] = "Post Updated"
+      redirect_to photos_path
+    else 
+      render 'edit'
+    end
   end
 
-  def destroy
+  def destroy 
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+      redirect_to users_path(current_user)
+  end
+
+  private
+ 
+  def photo_params
+    params.require(:photo).permit(:title, :image, :caption)
+  end
+
+  def check_logged_in
+  end
+
+  def set_photo
+    @photo = Photo.find(params[:id])
   end
 end
