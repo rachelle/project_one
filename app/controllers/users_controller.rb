c9ffba7d269c2d2642259a6691f01195f47e8b7a
Rachelle Herrera <rachelle.herera@gmail.com>
2015-09-16 18:31:08 -0700

    finished ux for sign up page and changed the routes when users signs up the will be redirected to their personal page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@
     @user = User.new(user_params)
     if @user.save 
       session[:user_id] = @user.id
-      redirect_to users_path
+      redirect_to user_path(current_user)
     else 
       render 'new'
     end
