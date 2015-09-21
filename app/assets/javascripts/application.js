34bb7a1b0e38425ef095c80b32889da605f36583
Rachelle Herrera <rachelle.herera@gmail.com>
2015-09-20 20:41:50 -0700

    added functionality for google maps locator

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,5 +16,71 @@
 //= require turbolinks
 //= require_tree .
 
+function gmap_show(user) {
+  if ((user.lat === null) || user.lng === null) ) { // validation check if coordinates are there
+     return 0; 
+  }
 
-   
+  handler = Gmaps.build('Google'); // map  init
+  handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
+    markers = handler.addMarkers({ // put marker method
+        {
+          "lat": user.lat,  //coordinates from paramater user
+          "lng": user.lng, 
+          "picture": { //setup marker icon
+          "url": 'http://www.planet-action.org/img/2009/interieur/icons/orange-dot.png',
+          "width": 32, 
+          "height": 32
+        },
+        "infowindow": "<b>" + user.name + "</b>" + user.address 
+
+      }
+    }); 
+    handler.bounds.extendWith(markers); 
+    handler.fitMapToBounds(); 
+    handler.getMap().setZoom(12); // set the default zoom of the map
+  });
+}
+
+function gmap_form(user) {
+  handler = Gmaps.build('Google'); // map init
+  handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
+    if (company && user.lat && user.lng) { // statement check - new or edit view 
+      markers = handler.addMarkers([ // print existent marker 
+        {
+          "lat": user.lat, 
+          "lng": user.lng, 
+          "picuture": {
+            "url": 'http://www.planet-action.org/img/2009/interieur/icons/orange-dot.png',
+            "width": 32, 
+            "height": 32
+          },
+          "infowindow": "<b>" + user.name + "</b>" + user.address
+        }
+      }); 
+      handler.bounds.extendWith(markers); 
+      handler.fitMapToBounds();
+      handler.getMap().setZoom(6); 
+    }
+  });
+
+  var makerOnMap; 
+
+  function placeMarker(location) { // simply method to put new marker on map
+    if (markerOnMap) {
+      markerOnMap.setPosition(location); 
+    }
+    else {
+      markerOnMap = new google.map.Marker({
+        position: location, 
+        map: handler.getMap()
+      });
+    }
+  }
+
+  google.maps.event.addListener(handler.getMap(), 'click', function(event) {
+    placeMarker(event.latlng); 
+    document.getElementById("map_lat").value = event.latlng.lat();
+    document.getElementById("map_lng").value = event.latlng.lng();
+  });
+} 
