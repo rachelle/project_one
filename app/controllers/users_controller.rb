d38d9caef0498404f9e363f5df6a81de18e9a5b9
Rachelle Herrera <rachelle.herera@gmail.com>
2015-09-22 08:06:06 -0700

    refactored search bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,13 @@
   
   def index
     @users = User.all
-    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
-        marker.lat user.latitude
-        marker.lng user.longitude
     @users = User.search(params[:search])
-     end
   end
+  @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+    marker.lat user.latitude
+    marker.lng user.longitude
+  end
+
   
   def new
     @user = User.new
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,11 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
-end
-
-def self.search(search)
-  if search 
-    find(:all, :condition => ['name LIKE?', "%{hair}%"])
-  else 
-    find(:all)
+  def self.search(search)
+    if search 
+      find(:all, :condition => ['name LIKE ?', "%#{seacrch}%"])
+    else 
+      find(:all)
+    end
   end
 end
