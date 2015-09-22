0c9da506b00465e0aca51df8e41fd910e3fd1f8f
Rachelle Herrera <rachelle.herera@gmail.com>
2015-09-22 12:34:58 -0700

    added update method for ratings controllerr

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,16 @@
 class RatingsController < ApplicationController
+
+def update
+    @rating = Rating.find(params[:id])
+    @comment = @rating.comment
+    if @rating.update_attributes(score: params[:score])
+      respond_to do |format|
+        format.js
+      end
+    end
+  end
+
 end
+
+
+
